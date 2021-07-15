dacebf9fc2cb5d52d20781a5effdd7984333935b
yokoto <youkotoushi@gmail.com>
2021-07-15 23:19:02 +0900

    add address edit page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,11 @@
 <template>
-  <hello-world />
+  <div class="home">
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+    name: 'home'
   }
 </script>
