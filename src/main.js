c03922bc901e500344069535b437ed0bd2a4fc60
yokoto <youkotoushi@gmail.com>
2021-07-17 00:01:01 +0900

    add firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBLkVwHBBQV89rhCsbgT7jtct5cB58x3gM",
+  authDomain: "my-address-pj-e83dd.firebaseapp.com",
+  projectId: "my-address-pj-e83dd",
+  storageBucket: "my-address-pj-e83dd.appspot.com",
+  messagingSenderId: "15533212214",
+  appId: "1:15533212214:web:0ac3d19034128b18face97"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
